9d63bae8fbe6aa6e2887c97318aa9cbcd7dfe8a9
Goostaf <gasplund2@gmail.com>
2026-08-19 09:42:06 +0200

    feat: Server action auth

--- src/app/actions/pubCrawls.ts
+++ src/app/actions/pubCrawls.ts
@@ -4,14 +4,23 @@ import { revalidatePath } from 'next/cache';
 import PubCrawlService, {
   type PubCrawlInput
 } from '@/services/pubCrawlService';
+import UserService from '@/services/userService';
 
 export async function createPubCrawl(input: PubCrawlInput) {
+  if (!(await UserService.getIsPRIT())) {
+    throw new Error('Unauthorized');
+  }
+
   const id = await PubCrawlService.createPubCrawl(input);
   revalidatePath('/pub-crawl');
   return id;
 }
 
 export async function updatePubCrawl(id: string, input: PubCrawlInput) {
+  if (!(await UserService.getIsPRIT())) {
+    throw new Error('Unauthorized');
+  }
+
   await PubCrawlService.updatePubCrawl(id, input);
   revalidatePath('/pub-crawl');
   revalidatePath(`/pub-crawl/${id}`);
